675269d78092e93fbf8c11e8f4d90769ad5ccf1d
Lewis Wall <lewall@microsoft.com>
2017-10-18 10:56:53 -0700

    Use $env:USERPROFILE so the dotfiles repo can be anywhere.

--- windows/Install-Stuff.ps1
+++ windows/Install-Stuff.ps1
@@ -1,6 +1,6 @@
 $me = $MyInvocation.MyCommand.Path
-$dir = split-Path $me
-New-Item -Force -ItemType Directory -Path $dir\..\..\tmp
-New-Item -Force -ItemType SymbolicLink -Path $dir\..\..\_vimrc -Value $dir\..\.vimrc
-New-Item -Force -ItemType SymbolicLink -Path $dir\..\..\vimfiles -Value $dir\..\.vim
+$dir = Split-Path $me
+New-Item -Force -ItemType Directory -Path $env:USERPROFILE\tmp
+New-Item -Force -ItemType SymbolicLink -Path $env:USERPROFILE\_vimrc -Value $dir\..\.vimrc
+New-Item -Force -ItemType SymbolicLink -Path $env:USERPROFILE\vimfiles -Value $dir\..\.vim
 New-Item -Force -ItemType SymbolicLink -Path $profile -Value $dir\Microsoft.Powershell_profile.ps1
